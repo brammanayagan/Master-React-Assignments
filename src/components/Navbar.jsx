9f07ed5031dcb13be4b879e0efd0d4209cce2886
Brammanayagan S <sakthibrammanayagan@gmail.com>
2026-04-08 12:28:43 +0530

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 bg-white/70 backdrop-blur-md border-b border-slate-200 shadow-sm text-slate-800 p-4 flex justify-between items-center transition-all duration-300">
-      <div className="text-2xl font-black tracking-tight text-blue-600 mx-4 md:mx-10 drop-shadow-sm">
+      <div className="text-2xl font-black tracking-tight text-black mx-4 md:mx-10 drop-shadow-sm">
         <Link to="/">BRAMMANAYAGAN S</Link>
       </div>
       <div className="hidden md:flex gap-6 mx-7 font-medium text-slate-600">
